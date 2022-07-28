1adda5e8bb8cb8f472aefc41cee10670ac5e8fd6
peterclermy <peterclermy232@gmail.com>
2022-07-28 15:59:54 +0300

    created home component

---
+++ .history/src/component/Home_20220728155914.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Products } from './Products'
+
+const Home = () => {
+    return (
+        <div className='hero'>
+            <div className="card bg-dark text-white border-0 ">
+                <img src="https://pps.whatsapp.net/v/t61.24694-24/167754377_4838834796203910_2704161392004520426_n.jpg?ccb=11-4&oh=01_AVwVMVUFzBuCbp8E9-Ukysw9fzrozhh_X5jDN2tbpccPIA&oe=62F1CF69" className="card-img" alt="Background " height="850px" />
+                <div className="card-img-overlay d-flex flex-column
+                justify-content-center">
+                    <div className="container">
+                    <h5 className="card-title display-3 fw-bolder mb-0">It is Peter's Time</h5>
+                   <p className="card-text lead fs-2">
+                    Welcome Customer from here you will what you like.
+                   </p>
+                    </div>
+                   
+                </div>
+            </div>
+            <Products />
+        </div>
+    )
+}
+
+export default Home
